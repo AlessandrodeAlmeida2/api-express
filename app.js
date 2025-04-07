fc2cdd9fb1f81c305e1148949558dd1d3316ad4f
Alessandro de Almeida <agostini55@gmail.com>
2025-04-07 16:27:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ app.get('/dados/:id', async (req, res) => {
     const { data, error } = await supabase
         .from('tabela1')
         .select('*')
-        .eq('id', id).single();
+        .eq('id', id);
 
     if (error) {
         return res.status(500).json({ error: 'Erro ao buscar o dado por ID' });
